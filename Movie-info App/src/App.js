f410082087d322a8de241e05777a36b89ca1f29d
Spartan-ash <ashishhsingh84@gmail.com>
2020-06-12 15:16:03 +0530

    Movies-info App

---
+++ Movie-info App/src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { HashRouter as Router, Route } from "react-router-dom";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+//import slider from "react-slick";
+
+import "./App.css";
+
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+
+import Landing from "./components/home/Landing";
+import Movie from "./components/home/Movie";
+
+import store from "./store";
+
+//-----------------
+// const photos = [{
+//   name : 'photo 1',
+//   url : "https://unsplash.com/photos/atsUqIm3wxo"
+// },{
+//   name : 'photo2',
+//   url : " https://unsplash.com/photos/aY3KgDPBVA4"
+// },{
+//   name : 'photo 3',
+//   url : "https://unsplash.com/photos/eVqU1HTZL8E "
+// },{
+//   name : 'photo 4',
+//   url : "https://unsplash.com/photos/jtmwD4i4v1U "
+// }]
+///-----------
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Navbar />
+            <div></div>
+
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/movie/:id" component={Movie} />
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ Movie-info App/src/components/layout/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="footer p-2 mt-4 text-center bg-light text-dark">
+            Created By@
+            <span className="text-success font-weight-normal">Ashish</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
